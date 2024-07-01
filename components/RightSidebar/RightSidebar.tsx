3047c7ad67160c5e97f2a5e9f718e966eb81ae7b
Sushil Magare <sushilmagare10@gmail.com>
2024-07-01 22:53:47 +0530

    updated rightsidebar for home

--- components/RightSidebar/RightSidebar.tsx
+++ components/RightSidebar/RightSidebar.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import FriendRequest from './FriendRequest'
+import Ad from '../Ad'
 
 const RightSidebar = () => {
     return (
-        <div>RightSidebar</div>
+        <div className=' flex flex-col gap-6'>
+            <FriendRequest />
+            <Ad />
+        </div>
     )
 }
 
