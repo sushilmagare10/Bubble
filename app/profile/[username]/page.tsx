39da95b8b4670575c07a2317d220049e61ad4cd5
Sushil Magare <sushilmagare10@gmail.com>
2024-07-02 23:32:24 +0530

    added user profile in profile page

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,6 +1,7 @@
 import Feed from '@/components/Feed/Feed'
 import LeftSidebar from '@/components/LeftSidebar/LeftSidebar'
 import RightSidebar from '@/components/RightSidebar/RightSidebar'
+import Image from 'next/image'
 import React from 'react'
 
 const ProfilePage = ({ params }: { params: { username: string } }) => {
@@ -11,6 +12,39 @@ const ProfilePage = ({ params }: { params: { username: string } }) => {
             </aside>
             <section className="w-full lg:w-[70%] xl:w-[50%]">
                 <div className="flex flex-col gap-8">
+                    <div className=' flex flex-col justify-center items-center'>
+                        <div className=' w-full h-64 relative'>
+                            <Image
+                                src='/banner.jpg'
+                                alt='banner'
+                                fill
+                                className=' rounded-lg object-cover '
+                            />
+                            <Image
+                                src='/profile3.png'
+                                alt='profile'
+                                quality={100}
+                                width={128}
+                                height={128}
+                                className=' rounded-full w-32 h-32 ring-4 absolute m-auto left-0 right-0 -bottom-16 ring-white object-cover'
+                            />
+                        </div>
+                        <h1 className=' mt-20 text-2xl text-center font-bold text-gray-800'>John Doe</h1>
+                        <div className='mt-10 flex justify-between items-center gap-16 '>
+                            <div className=' font-semibold flex flex-col justify-center items-center gap-1'>
+                                <span className='font-semibold text-lg'>150</span>
+                                <span className='font-medium'>Posts</span>
+                            </div>
+                            <div className=' font-semibold flex flex-col justify-center items-center gap-1'>
+                                <span className='font-semibold text-lg'>1.8k</span>
+                                <span className='font-medium'>Followers</span>
+                            </div>
+                            <div className=' font-semibold flex flex-col justify-center items-center gap-1'>
+                                <span className='font-semibold text-lg'>5.95k</span>
+                                <span className='font-medium'>Following</span>
+                            </div>
+                        </div>
+                    </div>
                     <Feed />
                 </div>
             </section>
