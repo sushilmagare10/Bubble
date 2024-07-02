40e4b558d3c2f66503165ecd65313d57f9f85a5b
Sushil Magare <sushilmagare10@gmail.com>
2024-07-02 22:19:33 +0530

    Added userInfocard in rightsidebar for user profile

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,11 +1,23 @@
+import Feed from '@/components/Feed/Feed'
 import LeftSidebar from '@/components/LeftSidebar/LeftSidebar'
+import RightSidebar from '@/components/RightSidebar/RightSidebar'
 import React from 'react'
 
 const ProfilePage = ({ params }: { params: { username: string } }) => {
     return (
-        <div className=' flex'>
-            <LeftSidebar />
-        </div>
+        <main className="flex gap-6 pt-6 ">
+            <aside className="hidden md:block xl:w-[20%] h-full sticky top-4">
+                <LeftSidebar type="profile" />
+            </aside>
+            <section className="w-full lg:w-[70%] xl:w-[50%]">
+                <div className="flex flex-col gap-8">
+                    <Feed />
+                </div>
+            </section>
+            <aside className="hidden lg:block w-[30%] h-full sticky top-4">
+                <RightSidebar userId='testId' />
+            </aside>
+        </main>
     )
 }
 
--- components/RightSidebar/RightSidebar.tsx
+++ components/RightSidebar/RightSidebar.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import FriendRequest from './FriendRequest'
 import Ad from '../Ad'
+import UserInfoCard from './UserInfoCard'
+import UserMediaCard from './UserMediaCard'
 
-const RightSidebar = () => {
+const RightSidebar = ({ userId }: { userId?: string }) => {
     return (
         <div className=' flex flex-col gap-6'>
+            {userId ? (
+                <>
+                    <UserInfoCard userId={userId} />
+                    <UserMediaCard userId={userId} />
+                </>
+            ) : null}
             <FriendRequest />
-            <Ad />
+            <Ad size='md' />
         </div>
     )
 }
