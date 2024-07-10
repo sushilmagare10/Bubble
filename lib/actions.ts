d32f70b140d2437659c518091107f1b82fec16e6
Sushil Magare <sushilmagare10@gmail.com>
2024-07-10 22:26:14 +0530

    Added Follow and Decline request

--- lib/actions.ts
+++ lib/actions.ts
@@ -93,3 +93,73 @@ export const switchBlock = async (userId: string) => {
 }
 
 
+export const acceptFollowRequest = async (userId: string) => {
+
+    const { userId: currentUserId } = auth()
+
+    if (!currentUserId) {
+        throw new Error("User not Authenticated")
+    }
+
+    try {
+
+        const existingFollowRequest = await prisma.followRequest.findFirst({
+            where: {
+                senderId: userId,
+                receiverId: currentUserId
+            }
+        })
+
+        if (existingFollowRequest) {
+            await prisma.followRequest.delete({
+                where: {
+                    id: existingFollowRequest.id
+                }
+            })
+
+            await prisma.follower.create({
+                data: {
+                    followerId: userId,
+                    followingId: currentUserId
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        throw new Error("Something went wrong")
+    }
+
+}
+
+
+
+export const declineFollowRequest = async (userId: string) => {
+
+    const { userId: currentUserId } = auth()
+
+    if (!currentUserId) {
+        throw new Error("User not Authenticated")
+    }
+
+    try {
+
+        const existingFollowRequest = await prisma.followRequest.findFirst({
+            where: {
+                senderId: userId,
+                receiverId: currentUserId
+            }
+        })
+
+        if (existingFollowRequest) {
+            await prisma.followRequest.delete({
+                where: {
+                    id: existingFollowRequest.id
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error)
+        throw new Error("Something went wrong")
+    }
+
+}
