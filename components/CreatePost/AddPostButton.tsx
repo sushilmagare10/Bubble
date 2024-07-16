1766c5fd82dbdccc79f6db326497a5a30004ba96
Sushil Magare <sushilmagare10@gmail.com>
2024-07-16 21:42:40 +0530

    Update Button Layout

--- components/CreatePost/AddPostButton.tsx
+++ components/CreatePost/AddPostButton.tsx
@@ -9,7 +9,7 @@ const AddPostButton = () => {
     const { pending } = useFormStatus()
 
     return (
-        <Button disabled={pending}>
+        <Button disabled={pending} className=' self-end'>
             {pending ? (
                 <div className="flex items-center gap-2">
                     <div className="inline-block h-[10px] w-[10px] animate-spin rounded-full border-2 border-white-300 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite]" />
