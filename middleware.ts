38e041782c184b3ba3cad3d9e39e9aacc1727053
Sushil Magare <sushilmagare10@gmail.com>
2025-05-31 20:56:17 +0530

    middleware err

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in", "/sign-up", "/api(.*)"]);
+const isProtectedRoute = createRouteMatcher(["/sign-up","sign-in",'api(.*)']);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) {
-    const { userId } = auth();
-
-    if (!userId) {
-      return auth().redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    // Check if user exists in your database
-    const host = req.headers.get("host");
-    const protocol = req.headers.get("x-forwarded-proto") ?? "http";
-    const baseUrl = `${protocol}://${host}`;
-    const response = await fetch(`${baseUrl}/api/check-user`, {
-      headers: {
-        "x-user-id": userId,
-      },
-    });
-
-    const { exists } = await response.json();
-
-    if (!exists && !req.url.includes("/sign-up")) {
-      return Response.redirect(`${baseUrl}/sign-up`, 302);
-    }
-  }
+export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
 });
 
 
 export const config = {
-    matcher: ["/((?!.*\\..*|_next|api|sign-in|sign-up).*)", "/"],
-  };
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
